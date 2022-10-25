4021177fb184f7b11f3df7ec065eb6ae1f84b53c
Лев Барабанщиков <levbara1@gmail.com>
2022-10-25 13:49:33 +0300

    Serializers done

---
+++ estate_market/serializers.py
@@ -0,0 +1,42 @@
+from estate_market.models import Customer, Seller, Ad, Flat, Type
+from rest_framework import serializers
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ['CustomerID', 'DateSignUp', 'FirstName', 'LastName']
+
+
+class SellerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seller
+        fields = ['SellerID', 'DateSignUp', 'FirstName', 'LastName', 'Telephone']
+
+
+class AdSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        fields = ['AdID',
+                  'Price',
+                  'CustomerID',
+                  'Title',
+                  'Description',
+                  'Status',
+                  'CreationDate',
+                  'SaleDate',
+                  'SellerID',
+                  'FlatID']
+
+
+class FlatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flat
+        fields = ['FlatID', 'Area', 'Address', 'Floor', 'TypeID', 'Balcony', 'YearBuilt',
+                  'BuildTech']
+
+
+class TypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Type
+        fields = ['TypeID', 'Name', 'NumBedrooms']
